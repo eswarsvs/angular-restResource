cc7570785a14c7e5b68c3fff3d8538ebb7b8e9f9
Aaron Reisman <aaron@lifeiscontent.net>
2013-10-31 11:59:16 -0700

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,28 @@
+{
+  "name": "angular-restResource",
+  "main": "angular-restResource.js",
+  "version": "0.0.1",
+  "homepage": "https://github.com/lifeiscontent/angular-restResource",
+  "description": "Angular Resource Module for REST API endpoints",
+  "keywords": [
+    "service",
+    "resource",
+    "api",
+    "REST",
+    "http",
+    "model",
+    "ngResource",
+    "$resource"
+  ],
+  "authors": [
+    "Aaron Reisman"
+  ],
+  "license": "MIT",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ]
+}
